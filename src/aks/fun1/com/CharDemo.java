0cc9eaa6808ad4d6a7f123bae399c7ebb44f5396
amitsupercomp <amitsupercomp@gmail.com>
2024-11-29 11:53:55 +0530

    "Add Binary"

---
+++ src/aks/fun1/com/CharDemo.java
@@ -0,0 +1,14 @@
+package aks.fun1.com;
+
+public class CharDemo {
+	
+	public static void main(String[] args) {
+		
+		char c = '1';
+		int i = 1;
+		
+		System.out.println((char)(i + '0'));
+		
+	}
+
+}
